9cc52f2aef253b185cc1c88324c895bfdb8d79f7
Maks-Siglov <gmb333x2@gmail.com>
2024-04-10 10:28:26 +0300

    PLox start, main class provided

---
+++ lox.py
@@ -0,0 +1,59 @@
+import sys
+import os
+
+
+class Lox:
+    had_error = False
+
+    @staticmethod
+    def run_file(filepath):
+        with open(filepath, "r") as file:
+            source = file.read()
+            Lox.run(source)
+
+    @staticmethod
+    def run_prompt():
+        while True:
+            try:
+                source = input("> ")
+                Lox.run(source)
+            except EOFError:
+                print("\nExiting...")
+                break
+
+    @staticmethod
+    def run(source):
+        print(f"Running {source}")
+
+    @staticmethod
+    def read_file(path):
+        try:
+            with open(path, "rb") as file:
+                bytes_content = file.read()
+                source = bytes_content.decode("utf-8")
+                Lox.run(source)
+        except FileNotFoundError:
+            print(f"Error: File '{path}' not found.")
+            sys.exit(66)
+        except IOError as e:
+            print(f"Error reading file '{path}': {e}")
+            sys.exit(74)
+
+    @staticmethod
+    def error(line, message):
+        Lox.report(line, "", message)
+
+    @staticmethod
+    def report(line, where, message):
+        print(f"[line {line}] Error{where}: {message}")
+        Lox.had_error = True
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 2:
+        print("Usage: python lox.py [script]")
+        sys.exit(64)
+    elif len(sys.argv) == 2:
+        Lox.read_file(sys.argv[1])
+    else:
+        Lox.run_prompt()
