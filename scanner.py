b8bf7258e05142cea3cb9113f22b888f9b8295c0
Maks-Siglov <gmb333x2@gmail.com>
2024-04-10 11:08:22 +0300

    Token provided, scanner start implemented

--- scanner.py
+++ scanner.py
@@ -1,6 +1,32 @@
+from token import Token
+from token_type import TokenType
+
+
 class Scanner:
     def __init__(self, source):
         self.source = source
+        self.tokens = []
+        self.start = 0
+        self.current = 0
+        self.line = 1
 
     def scan_tokens(self):
+        while not self.is_at_end():
+            self.start = self.current
+            self.scan_token()
+
+        self.tokens.append(
+            Token(
+                token_type=TokenType.EOF,
+                lexeme="",
+                literal=None,
+                line=self.line
+            )
+        )
+        return self.tokens
+
+    def is_at_end(self):
+        return self.current >= len(self.source)
+
+    def scan_token(self):
         pass
