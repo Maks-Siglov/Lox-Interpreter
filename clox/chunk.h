baccbab97333de514f9d42e63ad53a4439a93c9e
Maks-Siglov <gmb333x2@gmail.com>
2024-05-29 20:44:13 +0300

    Start of clox implementation

---
+++ clox/chunk.h
@@ -0,0 +1,23 @@
+#ifndef clox_chunk_h
+#define clox_chunk_h
+
+#include "common.h"
+
+typedef enum {
+    OP_RETURN,
+} OpCode;
+
+typedef struct {
+    int count;
+    int capacity;
+    uint8_t* code;
+} Chunk;
+
+void initChunk(Chunk* chunk){
+    chunk -> count = 0;
+    chunk -> capacity = 0;
+    chunk -> code = NULL;
+}
+
+
+#endif
