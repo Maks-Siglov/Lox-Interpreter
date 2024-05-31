832ae35353ba5b83a18ac926dca929386b4eacd3
Maks-Siglov <gmb333x2@gmail.com>
2024-05-31 17:52:43 +0300

    Stack for values in VM

--- clox/vm.c
+++ clox/vm.c
@@ -1,22 +1,37 @@
 #include <stdio.h>
 
 #include "common.h"
+#include "debug.h"
 #include "vm.h"
 
 VM vm;
 
-void initVM(){
+static void resetStack(){
+    vm.stackTop = vm.stack;
+}
 
+void initVM(){
+    resetStack();
 }
 
 void freeVM(){
 
 }
 
+void push(Value value){
+    *vm.stackTop = value;
+    vm.stackTop++;
+}
+
+Value pop(){
+    vm.stackTop--;
+    return *vm.stackTop;
+}
+
 InterpretResult interpret(Chunk* chunk){
     vm.chunk = chunk;
     vm.ip = vm.chunk -> code;
-    return run()
+    return run();
 }
 
 static InterpretResult run(){
@@ -24,15 +39,30 @@ static InterpretResult run(){
     #define READ_CONSTANT() (vm.chunk -> constants.values[READ_BYTE()])
 
     for(;;){
+        #ifdef DEBUG_TRACE_EXECUTION
+            printf(" ");
+            for(Value *slot=vm.stack; slot < vm.stackTop;slot++){
+                printf("[ ");
+                printValue(*slot);
+                printf(" ]");
+            }
+            printf("\n");
+
+            disassembleInstruction(vm.chunk, (int)(vm.ip - vm.chunk->code));
+        #endif
+
         uint8_t instruction;
         switch(instruction = READ_BYTE()){
             case OP_CONSTANT: {
                 Value constant = READ_CONSTANT();
+                push(constant);
                 printValue(constant);
                 printf("\n");
                 break;
             }
             case OP_RETURN: {
+                printValue(pop());
+                printf("\n");
                 return INTERPRET_OK;
             }
         }
