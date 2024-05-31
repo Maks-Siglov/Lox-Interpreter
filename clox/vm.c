ecf98c903d7d016d83c224a1c74092caa521a344
Maks-Siglov <gmb333x2@gmail.com>
2024-05-31 22:19:47 +0300

    Arithmetic opcodes provided

--- clox/vm.c
+++ clox/vm.c
@@ -38,6 +38,13 @@ static InterpretResult run(){
     #define READ_BYTE() (*vm.ip++)
     #define READ_CONSTANT() (vm.chunk -> constants.values[READ_BYTE()])
 
+    #define BINARY_OP(op) \
+        do { \
+            double b = pop(); \
+            double a = pop(); \
+            push(a op b); \
+        } while (false)
+
     for(;;){
         #ifdef DEBUG_TRACE_EXECUTION
             printf(" ");
@@ -64,6 +71,18 @@ static InterpretResult run(){
                 push(-pop());
                 break;
             }
+            case OP_ADD:
+                BINARY_OP(+);
+                 break;
+            case OP_SUBTRACT:
+                BINARY_OP(-);
+                break;
+            case OP_MULTIPLY:
+                BINARY_OP(*);
+                break;
+            case OP_DIVIDE:
+                BINARY_OP(/);
+                break;
             case OP_RETURN: {
                 printValue(pop());
                 printf("\n");
@@ -72,4 +91,5 @@ static InterpretResult run(){
         }
     }
     #undef READ_BYTE
+    #undef BINARY_OP
 }
