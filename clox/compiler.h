a83c0c02060cb4aac59650b9cf05ffda3ec0bdf9
Maks-Siglov <gmb333x2@gmail.com>
2024-06-01 17:52:33 +0300

    Compiler core implementation start

--- clox/compiler.h
+++ clox/compiler.h
@@ -1,6 +1,20 @@
 #ifndef clox_compiler_h
 #define clox_compiler_h
 
-void compile(const char* source);
+#include "vm.h"
+#include "scanner.h"
+
+bool compile(const char* source, Chunk* chunk);
+static void advance();
+static void expression();
+static void consume(TokenType type, const char* message);
+static void emitBytes(uint8_t byte1, uint8_t byte2);
+static void emitByte(uint8_t byte);
+static Chunk* currentChunk();
+static void endCompiler();
+static void emitReturn();
+static void errorAtCurrent(const char* message);
+static void error(const char* message);
+static void errorAt(Token* token, const char* message);
 
 #endif
