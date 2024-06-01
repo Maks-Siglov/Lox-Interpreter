2425da5aba7dd7b5056ad75f2b6388e1d4be0302
Maks-Siglov <gmb333x2@gmail.com>
2024-06-01 22:03:44 +0300

    Binary, unary, grouping expressions provided

--- clox/compiler.h
+++ clox/compiler.h
@@ -4,15 +4,46 @@
 #include "vm.h"
 #include "scanner.h"
 
+typedef enum {
+    PREC_NONE,
+    PREC_ASSIGNMENT, // =
+    PREC_OR, // or
+    PREC_AND, // and
+    PREC_EQUALITY, // == !=
+    PREC_COMPARISON, // < > <= >=
+    PREC_TERM, // + -
+    PREC_FACTOR, // * /
+    PREC_UNARY, // ! -
+    PREC_CALL, // . ()
+    PREC_PRIMARY
+} Precedence;
+
+typedef void (*ParseFn)();
+
+typedef struct {
+    ParseFn prefix;
+    ParseFn infix;
+    Precedence precedence;
+} ParseRule;
+
 bool compile(const char* source, Chunk* chunk);
+
 static void advance();
-static void expression();
 static void consume(TokenType type, const char* message);
 static void emitBytes(uint8_t byte1, uint8_t byte2);
 static void emitByte(uint8_t byte);
 static Chunk* currentChunk();
 static void endCompiler();
 static void emitReturn();
+static void expression();
+static void number();
+static void emitConstant(Value value);
+static uint8_t makeConstant(Value value);
+static void grouping();
+static void unary();
+static void binary();
+static void parsePrecedence(Precedence precedence);
+static ParseRule* getRule(TokenType type);
 static void errorAtCurrent(const char* message);
 static void error(const char* message);
 static void errorAt(Token* token, const char* message);
